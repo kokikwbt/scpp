fee4b74e5b809337853c3979e42fcb8f0744d8da
kouki kawabata <kouki@dm.cs.kumamoto-u.ac.jp>
2018-02-04 14:19:00 +0900

    update

--- scpp.py
+++ scpp.py
@@ -1,10 +1,16 @@
 import numpy as np
+import scipy.stats as st
+import scipy.special as sp
+from progressbar import ProgressBar
+
 from synthetic import *
+ZERO = 1.e-10
+INF = 1.e+10
+MAX_ITER = 1000
 
 global D, Z
 global n_items
 global n_users
-
 
 def C_u(gamma, user):
     C = 0.
@@ -16,80 +22,106 @@ def C_u(gamma, user):
     return C
 
 def M_i(item_id):
-    zin = Z[item_id]
-    return len(np.where(zin==0)[0])
+    return len(np.where(Z[item_id]==0)[0])
 
 def M_uu(user1, user2):
-    # ignore item -1, event -1
     cnt = 0
     for i in range(n_items):
-        zin = Z[i]
-        cause = np.where(zin==user1)[0]
-        events = D[i][cause, 0]
-        effect = np.where(events==user2)[0]
+        cause = np.where(Z[i]==user1)[0]
+        effect = np.where(D[i][cause, 0]==user2)[0]
         cnt += len(effect)
     return cnt
 
 def M_u(user):
-    # ignore item -1, event -1
-    cnt = 0
-    for i in range(n_items):
-        zin = Z[i]
-        cnt += len(np.where(zin==user)[0])
-    return cnt
+    return sum([len(np.where(Z[i]==user)[0]) for i in range(n_items)])
 
-def A_u3(gamma, user):
+def A_u0(gamma, user):
+    return gamma * C_u(gamma, user)
+# A'
+def A_u1(gamma, user):
     val = 0.
     for i in range(n_items):
         T = D[i][-1, 0]
         idx = np.where(D[i][:, 1] == user)[0]
         t = D[i][idx, 0] #.astype(np.float64)
-        val += np.exp(-gamma * (T - t)) * np.power((T - t), 2)
-    return val
-
+        val += np.sum(np.exp(-gamma * (T - t)) * (T - t))
+    return -1 * val
+# A''
 def A_u2(gamma, user):
     val = 0.
     for i in range(n_items):
-        T = D[i][-1, 0]
         idx = np.where(D[i][:, 1] == user)[0]
         t = D[i][idx, 0] #.astype(np.float64)
-        val += np.exp(-gamma * (T - t)) * (T - t)
-    return -val
-
-def A_u(gamma, user):
-    return gamma * C_u(user)
+        T = D[i][-1, 0]
+        val += np.sum(np.exp(-gamma * (T - t)) * np.power((T - t), 2))
+    return val
 
 def update_gamma(gamma, a, b):
-    val1 = val2 = val3 = 0.
+    deriv1 = deriv2 = 0.
     for i in range(n_items):
         idx = np.where(Z[i] > 0)[0]
-        val1 += np.sum(D[i][idx][:, 0] - D[i][Z[i][idx]][:, 0])
-    for user in range(n_users):
+        deriv1 += np.sum(D[i][idx][:, 0] - D[i][Z[i][idx]][:, 0])
+    for user in range(1, n_users):
         # compute components
         M = M_u(user)
-        Au = A_u(gamma, user)
-        Aup = A_u2(gamma, user)
-        Aupp = A_u3(gamma, user)
+        Au0 = A_u0(gamma, user)
+        Au1 = A_u1(gamma, user)
+        Au2 = A_u2(gamma, user)
+        val1 = (M + a) / (Au0 + np.power(gamma, b))
+        val2 = -1 * Au1 / gamma + Au1
         # for first derivative
-        tmp = M + a
-        tmp /= Au + gamma * b
-        hoge = -Au / gamma + Aup
-        tmp *= hoge
-        val2 += tmp
+        deriv1 += val1 * val2
         # for second derivative
-        tmp = M + a
-        tmp /= Au + gamma * b
-        tmp *= Aupp + hoge * ((Aup + b) / (Au + gamma * b) + 1 / gamma)
-        val3 += tmp
-    return gamma - (-(val1+val2)) / val3
+        deriv2 += val1 * (Au2 + val2 * ((Au1 + b) / (Au0 + np.power(gamma, b)) + 1 / gamma))
+    deriv1 *= -1
+    deriv2 *= -1
+    return gamma - deriv1 / deriv2
 
-def compute_lh():
-    return 0.
+def update_beta(beta):
+    term1 = term2 = 0.
+    for u0 in range(n_users):
+        for u1 in range(1, n_users):
+            term1 += sp.digamma(M_uu(u0, u1) + beta) - sp.digamma(beta)
+    for u in range(n_users):
+        term2 += sp.digamma(M_u(u) + beta * n_users) - sp.digamma(beta * n_users)
+    return beta * term1 / term2
 
-def E_step(gamma, beta, a, b):
-    for item_id in range(len(D)):
-        T = D[item_id][-1, 0]
-        for eid in range(1, len(D[item_id])): # zin of the first events is zero?
+def compute_lh(gamma, beta, a, b):
+    # the joint likelihood p(D, Z| gamma, beta, a, b)
+    llh = 0.
+    cnt = 0
+    for i in range(n_items):
+        events = D[i]
+        for n in range(1, len(events)):
+            zin = Z[i][n]
+            if zin == 0: continue
+            cnt += 1
+            llh += events[n, 0] - events[zin, 0]
+    print('# of cascade:', cnt)
+    llh = -gamma * llh
+    llh += (n_users + n_items) * np.log(np.power(b, a) / sp.gamma(a))
+    for u in range(1, n_users):
+        Mu = M_u(u)
+        llh += np.log(sp.gamma(Mu + a) / np.power(C_u(gamma, u) + b, Mu + a))
+    for i in range(n_items):
+        Mi = M_i(i)
+        llh += np.log(sp.gamma(Mi + a) / np.power(D[i][-1, 0] + b, Mi + a))
+    llh += (n_users + 1) * np.log(sp.gamma(beta * n_users) / np.power(sp.gamma(beta), n_users))
+    for u0 in range(n_users):
+        for u1 in range(1, n_users):
+            llh += np.log(sp.gamma(M_uu(u0, u1) + beta))
+        llh -= np.log(sp.gamma(M_u(u0) + beta * n_users))
+    return llh
+
+def sample_latent_index(gamma, beta, a, b):
+    progress = ProgressBar(0, n_items).start()
+    for item_id in range(n_items):
+        progress.update(item_id)
+        T = D[item_id][-1, 0] # Ci = T
+        for eid in range(len(D[item_id])):
+            if eid == 0:
+                Z[item_id][0] = 0
+                continue
             # reset z_in
             Z[item_id][eid] = -1
             pz = np.zeros(eid)
@@ -100,56 +132,52 @@ def E_step(gamma, beta, a, b):
             pz[0] *= M_uu(0, uin) + beta
             pz[0] /= M_u(0) + beta * n_users
             # otherwise
-            for i in range(1, eid - 1):
+            for i in range(1, eid):
                 tiy, uiy = D[item_id][i]
                 pz[i] = np.exp(-gamma * (tin - tiy))
                 pz[i] *= M_u(uiy) + a
                 pz[i] /= C_u(gamma, uiy) + b
                 pz[i] *= M_uu(uiy, uin) + beta
                 pz[i] /= M_u(uiy) + beta * n_users
-            denom = np.sum(pz)
-            pz = pz / denom
-            # sample latent index
+            pz = pz / np.sum(pz)
             Z[item_id][eid] = np.random.choice(np.arange(eid), size=1, p=pz)
-            print(Z[item_id][eid])
+    print('')
 
-def M_step():
-    # update Gamma
-
-    # update Beta
-
-    pass
-
-
-def inference(gamma=0.01, beta=1, a=1, b=1):
+def inference(gamma=10, beta=0.1, a=1, b=1):
     # stochastic EM algorithm
     lh = prev = 0.
-    for i in range(10000):
-        print('------')
-        print('E-step')
-        print('------')
-        E_step(gamma=gamma, beta=beta, a=a, b=b)
-
+    for i in range(MAX_ITER):
+        print('===============')
+        print('Iter: ', i + 1)
+        print('===============')
+        print('E-step:')
+        print('sampling latent indexes...')
+        # E step
+        sample_latent_index(gamma, beta, a, b)
         # M step
-        print(gamma)
+        print('M-step:')
+        prev = gamma
         gamma = update_gamma(gamma, a, b)
-        print('->', gamma)
-        exit()
-        # beta = update_beta()
-
-        if not i % 5:
-            lh = compute_lh()
-            print('Likelihood=', lh)
-            if np.fabs(lh - prev) < 10: break
+        print('Gamma: ', prev, '->', gamma)
+        if gamma < 0: gamma = ZERO
+        prev = beta
+        beta = update_beta(beta)
+        print('Beta: ', prev, '->', beta)
+        # if not i % 5:
+        prev = lh
+        lh = compute_lh(gamma, beta, a, b)
+        print('Likelihood =', prev)
+        print('Likelihood =', lh)
+            # if np.fabs(lh - prev) < 10: break
 
 
 if __name__ == '__main__':
 
-    n_items = 100
-    n_users = 100
+    n_items = 500
+    n_users = 20 + 1 # of users + background
     # marked point process
     D = import_data(n_items)
     # latent variable set
-    Z = [np.zeros(len(D[i]), dtype=np.int64) - 1 for i in range(n_items)]
+    Z = [np.zeros(len(D[i]), dtype=np.int64) for i in range(n_items)]
 
     inference()
--- synthetic.py
+++ synthetic.py
@@ -7,11 +7,23 @@ from numpy.random import *
 import matplotlib.pyplot as plt
 
 def marked_point_process(n_users):
-    n_events = randint(10, 50)
-    D, time = zeros((n_events, 2), dtype=int64), 0
+    n_events = randint(5, 10)
+    D = zeros((n_events, 2), dtype=int64)
     for i in range(n_events):
-        D[i, 0] = exponential(randint(30, 200)) + D[i - 1, 0]# time
-        D[i, 1] = randint(n_users) # user ID
+        user = -1
+        if i > 1:
+            if D[i - 1, 1] == 3:
+                user = choice([1, 4, 9], p=[0.2, 0.7, 0.1])
+            elif D[i - 1, 1] == 20:
+                user = 8
+            else:
+                user = randint(n_users)
+        else:
+            user = randint(n_users)
+        print(user)
+        D[i, 0] = exponential(200) + D[i - 1, 0]# time
+        D[i, 1] = user + 1 # user ID
+    print(D)
     return D
 
 def import_data(n_items):
@@ -20,11 +32,12 @@ def import_data(n_items):
 if __name__ == '__main__':
     if not len(sys.argv) == 3:
         exit('arg error')
-    n_items = int(sys.argv[1])
-    n_users = int(sys.argv[2])
-    if os.path.exists('./synthetic'):
-        shutil.rmtree('./synthetic')
-    os.mkdir('./synthetic')
-    for i in range(n_items):
-        D = marked_point_process(n_users)
-        savetxt('./synthetic/item' + str(i), D)
+    n_items = sys.argv[1]
+    n_users = sys.argv[2]
+    outdir = './synthetic_' + n_items + '_' + n_users + '/'
+    if os.path.exists(outdir):
+        shutil.rmtree(outdir)
+    os.mkdir(outdir)
+    for i in range(int(n_items)):
+        D = marked_point_process(int(n_users))
+        savetxt(outdir + 'item' + str(i), D)
